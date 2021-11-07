dc1329a477093e462da6a0dd978615cf635bc5e8
Katelyn <katelynjewel@gmail.com>
2021-11-06 17:42:28 -0700

    commit tree 50aca19e9401011344ea815176e2def32680afc1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+class Student < User
+
+    # @@knowledge = []
+
+    # def learn know
+    #     @@knowledge << know
+    # end
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
